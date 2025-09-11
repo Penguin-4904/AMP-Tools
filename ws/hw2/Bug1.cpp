a2116ce79ea3de7ea61b95661d12cb40e67cdfe0
Penguin-4904 <katrinajbraun@gmail.com>
2025-09-11 02:02:42 -0600

    Changed Bug1 perimeter following. Better computational efficiency, longer path length.

--- ws/hw2/Bug1.cpp
+++ ws/hw2/Bug1.cpp
@@ -11,6 +11,7 @@ amp::Path2D Bug1::plan(const amp::Problem2D& problem) {
 
     Eigen::Rotation2D<double> rot(M_PI / 36);
     Eigen::Rotation2D<double> rot90CW(-M_PI / 2);
+    Eigen::Rotation2D<double> rot90CCW(M_PI / 2);
 
     while (true){
         while (true){ // Move Towards Goal
@@ -30,11 +31,19 @@ amp::Path2D Bug1::plan(const amp::Problem2D& problem) {
         size_t q_leave_i = path.waypoints.size() - 1;
         double dist = (problem.q_goal - path.waypoints[q_hit_i]).norm();
         double best_dist = dist;
-
+        dir = rot90CCW * dir;
+        // perimeter following loop
         while(true){
-            // Turn away from obstacle to avoid collision and ensure 90 deg sensor can see obstacle
-            while(check_collisions(q_next, problem.obstacles) || !check_collisions((rot90CW * dir) * step_size + path.waypoints.back(), problem.obstacles)){ // Rotate left
-                dir = rot * dir;
+            if (!check_collisions((rot90CW * dir) * step_size + path.waypoints.back(), problem.obstacles)){
+                dir = rot90CW * dir;
+                q_next = dir * step_size + path.waypoints.back();
+            } else if (!check_collisions(dir * step_size + path.waypoints.back(), problem.obstacles)) {
+                q_next = dir * step_size + path.waypoints.back();
+            } else if (!check_collisions((rot90CCW * dir) * step_size + path.waypoints.back(), problem.obstacles)){
+                dir = rot90CCW * dir;
+                q_next = dir * step_size + path.waypoints.back();
+            } else {
+                dir = -dir;
                 q_next = dir * step_size + path.waypoints.back();
             }
             path.waypoints.push_back(q_next);
@@ -45,26 +54,9 @@ amp::Path2D Bug1::plan(const amp::Problem2D& problem) {
                 best_dist = dist;
             }
             // Check if returned to leave point
-            if (((path.waypoints.size() - q_hit_i) > 4) && (path.waypoints[q_hit_i] - path.waypoints.back()).norm() <= step_size){
+            if (((path.waypoints.size() - q_hit_i) > 3) && (path.waypoints[q_hit_i] - path.waypoints.back()).norm() <= step_size){
                 break;
             }
-            // Turn & move towards obstacle if robot left perimeter
-            while (!check_collisions((rot90CW * dir) * step_size + path.waypoints.back(), problem.obstacles)){
-                dir = rot90CW * dir;
-                path.waypoints.push_back(dir * step_size + path.waypoints.back());
-                // Check if point is closer to goal than all other points and store it if so.
-                dist = (problem.q_goal - path.waypoints.back()).norm();
-                if (dist < best_dist){
-                    q_leave_i = path.waypoints.size() - 1;
-                    best_dist = dist;
-                }
-            }
-            // Check if returned to leave point
-            if (((path.waypoints.size() - q_hit_i) > 4) && (path.waypoints[q_hit_i] - path.waypoints.back()).norm() <= step_size){
-                break;
-            }
-            // Reset q_next for next loop
-            q_next = dir * step_size + path.waypoints.back();
         }
 
         // Return to the closest leave point to goal via the fastest path
